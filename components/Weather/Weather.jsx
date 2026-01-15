cc4ac20577b0f66cdfbb699da89b27f7db3a45a9
Glenn Packer <glenn.packer.email@gmail.com>
2026-01-15 18:04:10 +0100

    tick

--- components/Weather/Weather.jsx
+++ components/Weather/Weather.jsx
@@ -154,7 +154,7 @@ export default function Weather({ events }) {
             insideCell = (
               <span title="Visit scheduled" className={weatherStyles['weather-tick-green']} style={{ display: 'inline-flex', alignItems: 'center' }}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ display: 'block' }}>
-                  <path d="M6 13L11 18L20 7" stroke="#2ecc40" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M6 13L11 18L20 7" stroke="#2ecc40" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </span>
             );
